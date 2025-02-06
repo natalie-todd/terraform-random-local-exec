53b3fefbf16388760ac8c2266c38385f0e9db912
Krista LaFentres <lafentres@users.noreply.github.com>
2025-02-06 11:50:31 -0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "random_id" "random" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  byte_length = 12
+
+  provisioner "local-exec" {
+    command = "python3 -m venv myenv; python3 ./test.py"
+  }
+}
+
+output "random" {
+  value = random_id.random.hex
+}
